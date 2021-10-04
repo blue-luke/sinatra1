f6a655c8f110708259cd73ff53ff819d7de4d592
luke <luke44@gmail.com>
2021-10-04 16:46:10 +0100

    Finished sinatra_start_and_restart

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
-gem 'ruby'
+# gem 'ruby' '3.0.0'
 gem 'sinatra', '~> 1.4', '>= 1.4.7'
+gem 'sinatra-reloader', '~> 1.0'
 gem 'webrick', '~> 1.3', '>= 1.3.1'
--- app.rb
+++ app.rb
@@ -4,3 +4,15 @@ require "sinatra/reloader" if development?
 get '/' do
   "Hello World"
 end
+
+get '/secret' do
+  'This is a DIFFERENT secret'
+end
+
+get '/public' do
+  "<h1>This is the public page</h1>"
+end
+
+get '/public/news' do
+  'Interest rates are up'
+end
